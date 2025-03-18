b599bd74d89148587990e1df3c1cda70055691c9
yuji <53387749+pierrejaune@users.noreply.github.com>
2025-03-18 12:46:27 +0900

    型ファイル追加

--- src/types.ts
+++ src/types.ts
@@ -7,3 +7,15 @@ export type Product = {
   likes: number;
   deadline?: string; // 期限日（YYYY-MM-DD 形式）
 };
+
+export type shops = {
+  id: string;
+  name: string;
+  shop_url: string;
+};
+
+export interface ProductDetail extends Product {
+  description: string;
+  deadline: string;
+  shops: shops[];
+}
